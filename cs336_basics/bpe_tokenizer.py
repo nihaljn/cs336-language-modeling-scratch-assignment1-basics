935d9cdbca88cc3b78d5e15d83938a9d01ca7f69
Nihal Jain <nihal.b.jain@gmail.com>
2025-08-11 02:10:29 -0400

    complete bpe training implementation

---
+++ cs336_basics/bpe_tokenizer.py
@@ -0,0 +1,183 @@
+import os
+from collections import Counter, defaultdict
+from functools import partial
+from multiprocessing import Pool
+
+from cs336_basics.pretokenization_example import pre_tokenize
+from tqdm import tqdm
+
+
+def get_new_word_representation(
+    word_representation: tuple[bytes], 
+    merged: bytes
+) -> tuple[bytes]:
+    # form new word representation
+    new_word_representation, skip_next = [], False
+    N = len(word_representation)
+    for i in range(N):
+        if skip_next:
+            skip_next = False
+            continue
+        if (i < N - 1) and word_representation[i]+word_representation[i+1] == merged:
+            # new token used for word
+            new_word_representation.append(merged) 
+            skip_next = True
+        else:
+            # old token used for word
+            new_word_representation.append(word_representation[i]) 
+    return tuple(new_word_representation)
+
+
+def process_word_with_merge(
+    word: str, 
+    new_token: bytes, 
+    word_to_tokens: dict[str, tuple[bytes]],
+    word_count: dict[str, int]
+) -> tuple:
+    old_word_representation = word_to_tokens[word]
+    new_word_representation = get_new_word_representation(
+        old_word_representation, new_token
+    )
+    # print(f"Before: {old_word_representation}\nAfter: {new_word_representation}\nReplacing: {other_pairs_to_replace}\n\n")
+
+    # new word representation brings new pairs
+    # 1. update pair_frequency
+    # 2. update pair_to_words
+    this_word_count = word_count[word]
+    this_pair_frequency = Counter()
+    for (b1, b2) in zip(new_word_representation[:-1], new_word_representation[1:]):
+        this_pair_frequency[(b1, b2)] += this_word_count
+    for (b1, b2) in zip(old_word_representation[:-1], old_word_representation[1:]):
+        this_pair_frequency[(b1, b2)] -= this_word_count
+
+    # send everything needed to apply update at global level
+    return (word, new_word_representation, this_pair_frequency)
+
+
+def train_bpe(
+    input_path: str,
+    vocab_size: int,
+    special_tokens: list[str],
+) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
+    """
+    Args:
+        input_path: str 
+            Path to a text file with BPE tokenizer training data.
+        vocab_size: int 
+            A positive integer that defines the maximum final vocabulary size 
+            (including the initial byte vocabulary, vocabulary items produced from 
+            merging, and any special tokens).
+        special_tokens: list[str] A list of strings to add to the vocabulary. 
+            These special tokens do not otherwise affect BPE training.
+    
+    Returns the resulting vocabulary and merges:
+        vocab: dict[int, bytes] 
+            The tokenizer vocabulary, a mapping from int (token ID in the vocabulary) 
+            to bytes (token bytes).
+        merges: list[tuple[bytes, bytes]] 
+            A list of BPE merges produced from training. Each list item
+            is a tuple of bytes (<token1>, <token2>), representing that 
+            <token1> was merged with <token2>. The merges should be ordered by 
+            order of creation.
+    """
+    # initialize vocab
+    vocab = {x: bytes((x,)) for x in range(256)}
+    merges = []
+    num_merges = vocab_size - (len(vocab) + len(special_tokens))
+
+    # pre-tokenize
+    word_count = pre_tokenize(input_path, special_tokens)
+    # initialize representation of word with byte tokens
+    word_tokens = {
+        k: tuple(bytes([x]) for x in k.encode("utf-8"))
+        for k in word_count
+    }
+    # start merging
+    # construct pair count table and pair to words cache
+    pair_to_words: dict[tuple[bytes, bytes], set[str]] = defaultdict(set)
+    pair_frequency: Counter[tuple[bytes, bytes], int] = Counter()
+    for word, v in word_count.items():
+        word_representation = word_tokens[word]
+        for (b1, b2) in zip(word_representation[:-1], word_representation[1:]):
+            pair_to_words[(b1, b2)].add(word)
+            pair_frequency[(b1, b2)] += v
+    
+    for i in tqdm(range(num_merges), desc="[bpe_tokenizer] Training BPE", ncols=100):
+        ####
+        # Algorithm:
+        # 1. find the most frequent pair and add to vocab
+        # 2. for all words that contained this pair:
+        #  a. form a new word representation
+        #  b. form new pairs and count their frequency in pair_frequency using word_count
+        #  c. update word_count to use new word representation
+        #  d. update pair_to_words to use new word representation
+        # 3. clean-up everything related to the pair
+        ####
+        
+        # step 1
+        # find most common pair
+        sorted_pairs = sorted(
+            pair_frequency.items(),
+            key=lambda x: (x[1], x[0]), # (count, lexicographical order)
+            reverse=True
+        )
+        this_merge: tuple[bytes, bytes] = sorted_pairs[0][0]
+        # merge and form new token
+        merges.append(this_merge)
+        new_token, new_token_idx = this_merge[0] + this_merge[1], len(vocab)
+        vocab[new_token_idx] = new_token
+        
+        # step 2
+        # update representation and stats for all words containing this_merge pair
+        target_words = sorted(pair_to_words[this_merge])
+        # next for loop can be parallelized
+        results = []
+        for word in target_words:
+            results.append(process_word_with_merge(
+                 word, new_token, word_tokens, word_count
+             ))
+        # func = partial(
+        #     process_word_with_merge,
+        #     new_token=new_token, 
+        #     word_to_tokens=word_tokens,
+        #     word_count=word_count
+        # )
+        # with Pool(processes=10) as p:
+        #     results = p.map(func, target_words)
+        
+        # reduce results globally
+        for res in results:
+            (word, new_word_representation, 
+             this_pair_frequency) = res
+            old_word_representation = word_tokens[word]
+            # perform updates
+            word_tokens[word] = new_word_representation
+            pair_frequency.update(this_pair_frequency)
+            for (b1, b2) in zip(old_word_representation[:-1], old_word_representation[1:]):
+                if word in pair_to_words[(b1, b2)]:
+                    # need to check above because (b1, b2) may have been 
+                    # removed in this iteration itself
+                    # e.g., 't','e','s','t','e','d' and we remove 't','e'
+                    # so it gets removed once already
+                    pair_to_words[(b1, b2)].remove(word)
+            for (b1, b2) in zip(new_word_representation[:-1], new_word_representation[1:]):
+                pair_to_words[(b1, b2)].add(word)
+
+        # step 3
+        # forget this token as a pair
+        del pair_to_words[this_merge]
+        del pair_frequency[this_merge]
+
+    # add special tokens to vocab
+    for i, token in enumerate(special_tokens):
+        vocab[i + len(vocab)] = token.encode("utf-8")
+    
+    return vocab, merges
+
+
+if __name__ == "__main__":
+    import sys
+    vocab, merges = train_bpe(sys.argv[1], int(sys.argv[2]), ["<|endoftext|>"])
+    print("Vocab size:", len(vocab))
+    print("Merges:", len(merges))
+    import ipdb; ipdb.set_trace()
--- cs336_basics/pretokenization_example.py
+++ cs336_basics/pretokenization_example.py
@@ -1,5 +1,11 @@
 import os
+from collections import Counter
+from functools import partial
+from multiprocessing import Pool
 from typing import BinaryIO
+
+import regex as re
+from tqdm import tqdm
 
 
 def find_chunk_boundaries(
@@ -22,7 +28,8 @@ def find_chunk_boundaries(
 
     # Initial guesses for chunk boundary locations, uniformly spaced
     # Chunks start on previous index, don't include last index
-    chunk_boundaries = [i * chunk_size for i in range(desired_num_chunks + 1)]
+    chunk_boundaries = [i * chunk_size 
+                        for i in range(desired_num_chunks + 1)]
     chunk_boundaries[-1] = file_size
 
     mini_chunk_size = 4096  # Read ahead by 4k bytes at a time
@@ -45,18 +52,62 @@ def find_chunk_boundaries(
                 break
             initial_position += mini_chunk_size
 
-    # Make sure all boundaries are unique, but might be fewer than desired_num_chunks
+    # Make sure all boundaries are unique, but might be fewer 
+    # than desired_num_chunks
     return sorted(set(chunk_boundaries))
 
 
-## Usage
-with open(..., "rb") as f:
-    num_processes = 4
-    boundaries = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
+def process_chunk(boundary: tuple[str, str], path=None, special_tokens=None) -> Counter:
+    start, end = boundary
+    PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
 
-    # The following is a serial implementation, but you can parallelize this
-    # by sending each start/end pair to a set of processes.
-    for start, end in zip(boundaries[:-1], boundaries[1:]):
+    with open(path, "rb") as f:
         f.seek(start)
-        chunk = f.read(end - start).decode("utf-8", errors="ignore")
-        # Run pre-tokenization on your chunk and store the counts for each pre-token
+        doc = f.read(end - start).decode("utf-8", errors="ignore")
+    # Run pre-tokenization on your chunk and store the counts 
+    # for each pre-tokens
+    chunk_strs = re.split("|".join(special_tokens), doc)
+    c = Counter()
+    for chunk_str in chunk_strs:
+        pre_tokens = re.findall(PAT, chunk_str)
+        c.update(pre_tokens)
+    return c
+
+
+def pre_tokenize(path: str, special_tokens: list, multiprocess: bool = True) -> Counter[str, int]:
+    print("[pretokenization_example] Reading from:", path)
+    num_processes = os.cpu_count()
+    with open(path, "rb") as f:
+        boundaries = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
+    
+    # Process
+    chunks = list(zip(boundaries[:-1], boundaries[1:]))
+    func = partial(process_chunk, path=path, special_tokens=special_tokens)
+    if multiprocess:
+        with Pool(processes=num_processes) as p:
+            pretoken_counts = list(tqdm(
+                p.imap_unordered(func, chunks), total=len(chunks),
+                desc="[pretokenization_example] Pre-tokenizing", ncols=100
+            ))
+    else:
+        pretoken_counts = []
+        for chunk in tqdm(chunks, desc="[pretokenization_example] Pre-tokenizing", ncols=100): 
+            pretoken_counts.append(func(chunk))
+    total_counts = Counter()
+    for c in pretoken_counts:
+        total_counts.update(c)
+    return total_counts
+
+
+if __name__ == "__main__":
+    ## Usage
+    import sys
+    path = sys.argv[1]
+    special_tokens = ["<|endoftext|>"]
+
+    total_counts = pre_tokenize(path, special_tokens, multiprocess=False)
+    sorted = total_counts.most_common()
+    print("Count of pre-tokens: ", len(sorted))
+    print("10 most common: ", sorted[:10])
+    print("10 least common: ", sorted[-10:])
+    print("10 random: ", sorted[23453:23463])
